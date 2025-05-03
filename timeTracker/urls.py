76116d4d30a6c517ce52b25bc55aeb5984232bd8
Alberto Ginel <aginelsalvador@gmail.com>
2025-05-03 21:48:33 +0300

    good template login

--- timeTracker/urls.py
+++ timeTracker/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from . import views
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import (
@@ -27,4 +28,5 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('activities/', include('activities.urls')),
+    path('', views.login_page, name='login_page'),
 ]
